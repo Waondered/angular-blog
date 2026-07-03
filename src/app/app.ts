9f2f756d8f747b148a638a7dc4cdc5c8dafa0b86
Wanderson <wwaonder.cruz@gmail.com>
2026-07-03 20:04:31 -0300

    feat: title component and other components's templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCard } from './components/small-card/small-card';
+import { BigCard } from './components/big-card/big-card';
+import { MenuBar } from './components/menu-bar/menu-bar';
+import { MenuTitle } from './components/menu-title/menu-title';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SmallCard,
+    BigCard,
+    MenuBar,
+    MenuTitle
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
